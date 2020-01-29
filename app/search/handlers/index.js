67ddd45f3510bf0189e8119e1dd2a26959f2277d
nathaly <nathzzpersia@gmail.com>
2020-01-29 11:36:06 -0400

    ElasticSearch sve the normalized tag from the recipe

--- app/search/handlers/index.js
+++ app/search/handlers/index.js
@@ -2,7 +2,7 @@ const amphoraSearch = require('amphora-search'),
   highland = require('highland'),
   { getComponentName } = require('clayutils');
 
-const elasticIndex = 'local_index_v1';
+const elasticIndex = 'local_recipes_index';
 
 amphoraSearch.subscribe('save').through(handlerSave);
 
@@ -17,18 +17,20 @@ function handlerSave(stream) {
     .parallel(1)
     .flatten()
     .map(element => {
-      const { recipeTitle, canonicalUrl, imgBigUrl, cantMadeIt } = JSON.parse(element.value);
+      const { recipeTitle, canonicalUrl, imgBigUrl, cantMadeIt, normalizedTags } = JSON.parse(
+        element.value
+      );
       return {
         _ref: element.key,
         recipeTitle,
         canonicalUrl,
         imgBigUrl,
-        cantMadeIt
+        cantMadeIt,
+        normalizedTags
       };
     })
     .flatMap(op => {
       return highland(amphoraSearch.elastic.put(elasticIndex, op._ref, op).then(() => op._ref));
-    });
-  // .each()
-  // .error();
+    })
+    .done();
 }
