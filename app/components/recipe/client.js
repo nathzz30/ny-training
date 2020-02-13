55d9aaa09da618ca895f00ff5ce6ec459b5fd446
nathaly <nathzzpersia@gmail.com>
2020-02-13 11:59:07 -0400

    Slide show logic implemented

--- app/components/recipe/client.js
+++ app/components/recipe/client.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const sharePopUp = require('../../services/client/sharePopup');
+module.exports = el => {
+  let images = document.getElementsByClassName('image');
+  let activeImage = document.getElementsByClassName('active');
+  activeImage[0].src = images[0].src;
 
-// module.exports = el => {
-//   console.log('EL: ', el);
-// };
+  for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', e => {
+      activeImage[0].src = e.target.src;
+    });
+  }
+};
